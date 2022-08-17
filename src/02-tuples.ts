12bb118302746a47d77161ef11d77de8d0ba01ff
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2022-08-17 13:15:33 -0500

    feat: Clase 02-tuples

---
+++ src/02-tuples.ts
@@ -0,0 +1,9 @@
+const prices: (number | string)[] = [1, 2, 3];
+
+let user: [string, number, boolean];
+user = ['gustavo', 15, true];
+
+const [username, age] = user;
+
+console.log(username);
+console.log(age);
