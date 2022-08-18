776f0c16025c30f311d15399503c802b0493ebd7
gustmg <gustavo.mitre.gallardo@gmail.com>
2022-08-17 20:54:32 -0500

    Clase interfaces extendidas

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,10 +4,13 @@ addProduct({
   id: 1,
   title: 'p1',
   createdAt: new Date(),
+  updatedAt: new Date(),
   stock: 0,
   size: 'G',
   category: {
     id: 12,
     name: 'c1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
 });
